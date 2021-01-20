65829d156614b7dba62facfee8bcc7437391349d
Tio Soneca <thalesfilho1@hotmail.com>
2021-01-20 16:46:00 -0300

    Update nowplaying.js

--- src/commands/nowplaying.js
+++ src/commands/nowplaying.js
@@ -2,7 +2,7 @@ const util = require("../util");
 
 module.exports = {
     name: "nowplaying",
-    aliases: ["np", "nowplay"],
+    aliases: ["np", "tocandoagora"],
     exec: (msg) => {
         const { music } = msg.guild;
         if (!music.player || !music.player.playing) return msg.channel.send(util.embed().setDescription("❌ | Currently not playing anything."));
