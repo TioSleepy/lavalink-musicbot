55914f4873d929bfe268c066f26cb72758f09fc6
Tio Soneca <thalesfilho1@hotmail.com>
2021-01-20 16:10:11 -0300

    tradu

--- src/commands/volume.js
+++ src/commands/volume.js
@@ -6,18 +6,18 @@ module.exports = {
     exec: async (msg, args) => {
         const { music } = msg.guild;
         const newVolume = parseInt(args[0], 10);
-        if (!music.player || !music.player.playing) return msg.channel.send(util.embed().setDescription("❌ | Currently not playing anything."));
+        if (!music.player || !music.player.playing) return msg.channel.send(util.embed().setDescription("❌ | Atualmente não estou tocando nada."));
         try {
             if (isNaN(newVolume)) {
                 msg.channel.send(util.embed().setDescription(`🔉 | Current volume \`${music.volume}\`.`));
             } else {
                 if (!msg.member.voice.channel)
-                    return msg.channel.send(util.embed().setDescription("❌ | You must be on a voice channel."));
+                    return msg.channel.send(util.embed().setDescription("❌ | Você deve estar em um canal de voz!."));
                 if (msg.guild.me.voice.channel && !msg.guild.me.voice.channel.equals(msg.member.voice.channel))
-                    return msg.channel.send(util.embed().setDescription(`❌ | You must be on ${msg.guild.me.voice.channel} to use this command.`));
+                    return msg.channel.send(util.embed().setDescription(`❌ | Você deve estar em ${msg.guild.me.voice.channel} para usar esse comando`));
 
                 if (newVolume < 0 || newVolume > 150)
-                    return msg.channel.send(util.embed().setDescription("❌ | You can only set the volume from 0 to 150."));
+                    return msg.channel.send(util.embed().setDescription("❌ | Você so pode colocar o volume de 0 a 150%"));
 
                 await music.setVolume(newVolume);
                 msg.channel.send(util.embed().setDescription(`🔉 | Volume set to \`${music.volume}\`.`));
