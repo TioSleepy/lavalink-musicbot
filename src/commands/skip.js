c0c38a8c78e72cad01eef2048414b4371f9a7563
Tio Soneca <thalesfilho1@hotmail.com>
2021-01-20 16:14:49 -0300

    trad

--- src/commands/skip.js
+++ src/commands/skip.js
@@ -4,11 +4,11 @@ module.exports = {
     name: "skip",
     exec: async (msg) => {
         const { music } = msg.guild;
-        if (!music.player || !music.player.playing) return msg.channel.send(util.embed().setDescription("❌ | Currently not playing anything."));
+        if (!music.player || !music.player.playing) return msg.channel.send(util.embed().setDescription("❌ | Você deve estar em um canal de voz."));
         if (!msg.member.voice.channel)
-            return msg.channel.send(util.embed().setDescription("❌ | You must be on a voice channel."));
+            return msg.channel.send(util.embed().setDescription("❌ | Você deve estar em um canal de voz."));
         if (msg.guild.me.voice.channel && !msg.guild.me.voice.channel.equals(msg.member.voice.channel))
-            return msg.channel.send(util.embed().setDescription(`❌ | You must be on ${msg.guild.me.voice.channel} to use this command.`));
+            return msg.channel.send(util.embed().setDescription(`❌ | Você deve estar no canal ${msg.guild.me.voice.channel} para usar esse comando.`));
 
         try {
             await music.skip();
