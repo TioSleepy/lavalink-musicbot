b322ebb5beb480f0df7f35f63f280ce8fb910388
Tio Soneca <thalesfilho1@hotmail.com>
2021-01-20 16:16:24 -0300

    trad

--- src/commands/shuffle.js
+++ src/commands/shuffle.js
@@ -5,15 +5,15 @@ module.exports = {
     aliases: ["sf"],
     exec: async (msg) => {
         const { music } = msg.guild;
-        if (!music.player || !music.player.playing) return msg.channel.send(util.embed().setDescription("❌ | Currently not playing anything."));
-        if (!music.queue.length) return msg.channel.send(util.embed().setDescription("❌ | Queue is empty."));
+        if (!music.player || !music.player.playing) return msg.channel.send(util.embed().setDescription("❌ | Atualmente não estou tocando nada"));
+        if (!music.queue.length) return msg.channel.send(util.embed().setDescription("❌ | Queue vazia."));
         if (!msg.member.voice.channel)
-            return msg.channel.send(util.embed().setDescription("❌ | You must be on a voice channel."));
+            return msg.channel.send(util.embed().setDescription("❌ | Você deve estar em um canal de voz."));
         if (msg.guild.me.voice.channel && !msg.guild.me.voice.channel.equals(msg.member.voice.channel))
-            return msg.channel.send(util.embed().setDescription(`❌ | You must be on ${msg.guild.me.voice.channel} to use this command.`));
+            return msg.channel.send(util.embed().setDescription(`❌ | Você deve estar no canal ${msg.guild.me.voice.channel} para usar esse comando.`));
 
         music.queue = util.shuffleArray(music.queue);
 
-        msg.channel.send(util.embed().setDescription(`✅ | Queue shuffled! Type \`${msg.client.prefix}queue\` to see changes.`));
+        msg.channel.send(util.embed().setDescription(`✅ | Queue Misturada! Digite \`${msg.client.prefix}queue\` para ver mudanças.`));
     }
 };
