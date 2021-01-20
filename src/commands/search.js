932e11c09c6dadfb212f188d435e6907d5eb5a6f
Tio Soneca <thalesfilho1@hotmail.com>
2021-01-20 16:25:48 -0300

    tradu

--- src/commands/search.js
+++ src/commands/search.js
@@ -5,23 +5,23 @@ module.exports = {
     exec: async (msg, args) => {
         const { music } = msg.guild;
         if (!msg.member.voice.channel)
-            return msg.channel.send(util.embed().setDescription("❌ | You must be on a voice channel."));
+            return msg.channel.send(util.embed().setDescription("❌ | Você deve estar em um canal de voz."));
         if (msg.guild.me.voice.channel && !msg.guild.me.voice.channel.equals(msg.member.voice.channel))
-            return msg.channel.send(util.embed().setDescription(`❌ | You must be on ${msg.guild.me.voice.channel} to use this command.`));
+            return msg.channel.send(util.embed().setDescription(`❌ | Você deve estar no canal ${msg.guild.me.voice.channel} para usar esse comando.`));
 
         const missingPerms = util.missingPerms(msg.guild.me.permissionsIn(msg.member.voice.channel), ["CONNECT", "SPEAK"]);
         if ((!music.player || !music.player.playing) && missingPerms.length)
-            return msg.channel.send(util.embed().setDescription(`❌ | I need ${missingPerms.length > 1 ? "these" : "this"} permission${missingPerms.length > 1 ? "s" : ""} on your voice channel: ${missingPerms.map(x => `\`${x}\``).join(", ")}.`));
+            return msg.channel.send(util.embed().setDescription(`❌ | Eu preciso de ${missingPerms.length > 1 ? "essas" : "essa"} permis${missingPerms.length > 1 ? "são" : "sões"} nesse canal de voz: ${missingPerms.map(x => `\`${x}\``).join(", ")}.`));
 
         if (!music.node || !music.node.connected)
-            return msg.channel.send(util.embed().setDescription("❌ | Lavalink node not connected."));
+            return msg.channel.send(util.embed().setDescription("❌ | Servidor principal desligado, favor contatar o criador do bot Tio#7717."));
 
         const query = args.join(" ");
-        if (!query) return msg.channel.send(util.embed().setDescription("❌ | Missing args."));
+        if (!query) return msg.channel.send(util.embed().setDescription("❌ | Faltam Argumentos."));
 
         try {
             let { tracks } = await music.load(`ytsearch:${query}`);
-            if (!tracks.length) return msg.channel.send(util.embed().setDescription("❌ | Couldn't find any results."));
+            if (!tracks.length) return msg.channel.send(util.embed().setDescription("❌ | Não encontrei nada."));
 
             tracks = tracks.slice(0, 10);
 
@@ -31,20 +31,20 @@ module.exports = {
                 .setFooter("Select from 1 to 10 or type \"cancel\" to cancel the command."));
 
             const collector = await awaitMessages();
-            if (!collector) return resultMessage.edit(util.embed().setDescription("❌ | Time is up!"));
+            if (!collector) return resultMessage.edit(util.embed().setDescription("❌ | Tempo expirado!"));
             const response = collector.first();
 
             if (response.deletable) response.delete();
 
             if (/^cancel$/i.exec(response.content))
-                return resultMessage.edit(util.embed().setDescription("✅ | Cancelled."));
+                return resultMessage.edit(util.embed().setDescription("✅ | Cancelado!"));
 
             const track = tracks[response.content - 1];
             track.requester = msg.author;
             music.queue.push(track);
 
             if (music.player && music.player.playing) {
-                resultMessage.edit(util.embed().setDescription(`✅ | **${track.info.title}** added to the queue.`));
+                resultMessage.edit(util.embed().setDescription(`✅ | **${track.info.title}** Adicionada a lista.`));
             } else {
                 resultMessage.delete();
             }
