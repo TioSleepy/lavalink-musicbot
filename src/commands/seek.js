d1815eae53565161328f65a50fa20fba3a2f2aee
Tio Soneca <thalesfilho1@hotmail.com>
2021-01-20 16:21:17 -0300

    tradu

--- src/commands/seek.js
+++ src/commands/seek.js
@@ -6,28 +6,28 @@ module.exports = {
     name: "seek",
     exec: async (msg, args) => {
         const { music } = msg.guild;
-        if (!music.player || !music.player.playing) return msg.channel.send(util.embed().setDescription("❌ | Currently not playing anything."));
+        if (!music.player || !music.player.playing) return msg.channel.send(util.embed().setDescription("❌ | Atualmente não estou tocando nada."));
         if (!msg.member.voice.channel)
-            return msg.channel.send(util.embed().setDescription("❌ | You must be on a voice channel."));
+            return msg.channel.send(util.embed().setDescription("❌ | Você deve estar em um canal de voz."));
         if (msg.guild.me.voice.channel && !msg.guild.me.voice.channel.equals(msg.member.voice.channel))
-            return msg.channel.send(util.embed().setDescription(`❌ | You must be on ${msg.guild.me.voice.channel} to use this command.`));
+            return msg.channel.send(util.embed().setDescription(`❌ | Você deve estar no canal ${msg.guild.me.voice.channel} para usar esse comando.`));
 
         if (!music.current.info.isSeekable)
-            return msg.channel.send(util.embed().setDescription("❌ | Current track isn't seekable."));
+            return msg.channel.send(util.embed().setDescription("❌ | A musica atual não pode ser avançada."));
 
         const duration = args[0];
         if (!duration)
-            return msg.channel.send(util.embed().setDescription("❌ | You must provide duration to seek. Valid duration e.g. `1:34`."));
+            return msg.channel.send(util.embed().setDescription("❌ | Você deve prover um tempo valido. Exemplo `1:34`."));
         if (!durationPattern.test(duration))
-            return msg.channel.send(util.embed().setDescription("❌ | You provided an invalid duration. Valid duration e.g. `1:34`."));
+            return msg.channel.send(util.embed().setDescription("❌ | Você deve prover um tempo valido. Exemplo `1:34`."));
 
         const durationMs = util.durationToMillis(duration);
         if (durationMs > music.current.info.length)
-            return msg.channel.send(util.embed().setDescription("❌ | The duration you provide exceeds the duration of the current track."));
+            return msg.channel.send(util.embed().setDescription("❌ | A musica atual é menor que o tempo informado. "));
 
         try {
             await music.player.seek(durationMs);
-            msg.channel.send(util.embed().setDescription(`✅ | Seeked to ${util.millisToDuration(durationMs)}.`));
+            msg.channel.send(util.embed().setDescription(`✅ | Musica avançada para ${util.millisToDuration(durationMs)}.`));
         } catch (e) {
             msg.channel.send(`An error occured: ${e.message}.`);
         }
